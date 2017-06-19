cec0293c4f67b73ba77574c8fd1bf58dafbb56b6
Pesanteur <pesanteur123@gmail.com>
2017-06-19 17:36:29 -0400

    Added csv generation option

---
+++ FaStA/tweepy/pd_analysis.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv('2017-06-19-realDonaldTrump-sentimentanalysis.csv')
+df.describe()
--- FaStA/tweepy/tweet_analysis.py
+++ FaStA/tweepy/tweet_analysis.py
@@ -1,17 +1,34 @@
 from oauth import login
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from datetime import date
+import csv
 
 analyzer = SentimentIntensityAnalyzer()
 api = login()[2]
 
-timeline = api.user_timeline(screen_name="realDonaldTrump",count=50)
+screen_name = "realDonaldTrump"
+today = date.today()
 
-count = 0
-for tweet in timeline:
-    if "RT" in tweet.text:
-        continue
-    vs = analyzer.polarity_scores(tweet.text)
-    negative = vs['neg']
-    retweet_count = tweet.retweet_count
-    print("%s.\t%s\t%s\t%s" % (count, tweet.text, negative, retweet_count))
-    count += 1
+timeline = api.user_timeline(screen_name=screen_name,count=1000)
+
+count = 1
+
+fields = ['Number', 'Tweet Text', 'Negative Score', 'Positive Score', 'Retweet Count', 'Like Count']
+
+print("Now gathering tweet data for %s..." % screen_name)
+
+with open('%s-%s-sentimentanalysis.csv' % (today, screen_name), 'w') as stats:
+    stats_writer = csv.writer(stats)
+    stats_writer.writerow(fields)
+
+    for tweet in timeline:
+        if "RT" in tweet.text:
+            continue
+        vs = analyzer.polarity_scores(tweet.text)
+        negative = vs['neg']
+        positive = vs['pos']
+        like_count = tweet.favorite_count
+        retweet_count = tweet.retweet_count
+        print("%s.\t%s\t%s\t%s\t%s\t%s" % (count, tweet.text, negative, positive, retweet_count, like_count))
+        stats_writer.writerow([count, tweet.text, negative, positive, retweet_count, like_count])
+        count += 1

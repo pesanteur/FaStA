664406773e602e7edd3ef304f245ae43c0717420
Pesanteur <pesanteur123@gmail.com>
2017-06-20 17:01:13 -0400

    Added code to parse through more tweets

--- FaStA/tweepy/tweet_analysis.py
+++ FaStA/tweepy/tweet_analysis.py
@@ -2,6 +2,7 @@ from oauth import login
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from datetime import date
 import csv
+import tweepy
 
 analyzer = SentimentIntensityAnalyzer()
 api = login()[2]
@@ -9,7 +10,7 @@ api = login()[2]
 screen_name = "realDonaldTrump"
 today = date.today()
 
-timeline = api.user_timeline(screen_name=screen_name,count=1000)
+#timeline = api.user_timeline(screen_name=screen_name,count=1000)
 
 count = 1
 
@@ -21,14 +22,19 @@ with open('%s-%s-sentimentanalysis.csv' % (today, screen_name), 'w') as stats:
     stats_writer = csv.writer(stats)
     stats_writer.writerow(fields)
 
-    for tweet in timeline:
-        if "RT" in tweet.text:
-            continue
-        vs = analyzer.polarity_scores(tweet.text)
-        negative = vs['neg']
-        positive = vs['pos']
-        like_count = tweet.favorite_count
-        retweet_count = tweet.retweet_count
-        print("%s.\t%s\t%s\t%s\t%s\t%s" % (count, tweet.text, negative, positive, retweet_count, like_count))
-        stats_writer.writerow([count, tweet.text, negative, positive, retweet_count, like_count])
-        count += 1
+    page_list = []
+    for page in tweepy.Cursor(api.user_timeline, count=200, id=screen_name).pages(16):
+        page_list.append(page)
+
+    for page in page_list:
+        for tweet in page:
+            if "RT" in tweet.text:
+                continue
+            vs = analyzer.polarity_scores(tweet.text)
+            negative = vs['neg']
+            positive = vs['pos']
+            like_count = tweet.favorite_count
+            retweet_count = tweet.retweet_count
+            print("%s.\t%s\t%s\t%s\t%s\t%s" % (count, tweet.text, negative, positive, retweet_count, like_count))
+            stats_writer.writerow([count, tweet.text, negative, positive, retweet_count, like_count])
+            count += 1
